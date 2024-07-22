936e69cf1f3fc1a970fdbd480b64fe1abd6893a2
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2024-07-22 19:05:38 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import WheelComponent from "./components/WheelComponent";
 
-function App() {
+export default function App() {
+  const segments = ["Pierpaolo", "Gianmarco", "Andrea", "Gabriele", "Lorenzo", "Emanuele", "Benedetta"];
+  const segColors = [
+    "#EE4040",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    "#34A24F",
+    "#F9AA1F",
+    "#FF9000"
+  ];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wheelCircle">
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          winningSegment=""
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          primaryColoraround="#ffffffb4"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={50}
+          downDuration={2000}
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
